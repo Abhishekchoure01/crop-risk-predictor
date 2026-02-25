1cd320e07420eb90731350cf76958384c7b7d801
Abhishekchoure01 <abhishekchoure.512@gmail.com>
2026-02-26 01:35:21 +0530

    Add __init__.py to make src a proper Python package

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Crop Risk Predictor - src package
+
+Modules:
+    - data_gen: Generate synthetic crop weather data
+    - model: CropYieldLossModel for yield loss prediction
+    - risk_logic: Risk assessment and explanation functions
+    - app: Gradio web interface
+"""
+
+__version__ = "0.1.0"
